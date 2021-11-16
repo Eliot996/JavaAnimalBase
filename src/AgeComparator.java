0ae92b58161f284b0de2cf585fcbbf299ddbb3e6
Eliot996 <math10t6@stud.kea.dk>
2021-11-16 13:50:26 +0100

    changed from simple subtraction to Interger.compare()

--- src/AgeComparator.java
+++ src/AgeComparator.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class AgeComparator implements Comparator<Animal> {
     @Override
     public int compare(Animal o1, Animal o2) {
-        return o1.getAge() - o2.getAge();
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
